b02261bbce7b4d9c9bf65a51ba00534faf21c73b
pedrodominguesquaio <00001126901957sp@al.educacao.sp.gov.br>
2024-08-09 20:07:28 -0300

    Create scketch.js

---
+++ scketch.js
@@ -0,0 +1,27 @@
+et cor
+let cy
+function setup() 
+  createCanvas(400, 400);
+  background("yellow")
+ cor= color(150,0,245)
+cor= color  (random(0,255),random(0,255),random(0,255),
+circuloy=(random(height),random(height),random(height)
+  circulox= [0,0,0,];
+}             
+function draw () {
+  fill (cor);
+  for (let quantos in circulox) {
+  
+  
+  circle(circulox)[quantos],circuloY[quantos],100);
+circuloX[quantos]+=random(0,3);
+circuloY[quantos]+=random(-3,3);
+if(circuloX)[quantos]
+    
+
+function draw()
+  circle(200,200,30)
+  fill(cor)
+    
+  
+}
